1222caad152abbeb81be86bbdcac1c550cdc717a
Ethan <riley787cmplex@gmail.com>
2018-05-18 15:53:13 +0100

    add problem 4

--- 99problems.hs
+++ 99problems.hs
@@ -1,8 +1,7 @@
 myLast :: [a] -> a
 myLast [] = error "can't find last of empty list"
 myLast [x] = x
-myLast (x:xs) = 
-    myLast xs
+myLast (x:xs) = myLast xs
 
 myButLast :: [a] -> a
 myButLast [] = error "can't find penultimate of empty list"
@@ -12,5 +11,9 @@ myButLast (x:xs)
     | otherwise = myButLast xs
 
 elementAt :: [a] -> Int -> a
-elementAt (x:xs) 0 = x
+elementAt (x:xs) 1 = x
 elementAt (x:xs) n = elementAt xs (n-1)
+
+myLength :: [a] -> Int 
+myLength [] = 0
+myLength (x:xs) = 1 + myLength xs
